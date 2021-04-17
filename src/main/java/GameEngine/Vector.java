22706ba8e86be4a0a3b5273bcb73196033fc0ef2
Leonard Langer <syntaxzockt@gmail.com>
2021-04-17 19:05:55 +0200

    Added Vector printing ^^

--- src/main/java/GameEngine/Vector.java
+++ src/main/java/GameEngine/Vector.java
@@ -3,6 +3,23 @@ package GameEngine;
 public class Vector {
     double x;
     double y;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
     public Vector(double x, double y) {
         this.x = x;
         this.y = y;
