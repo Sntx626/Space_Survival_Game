7cae1a1f3c5516718038e6944d40d170c303ceeb
Alexschlomo <alexschlomo@gmail.com>
2021-04-17 15:09:44 +0200

    Projectile

---
+++ src/main/java/GameEngine/World/Projectiles/Laser.java
@@ -0,0 +1,8 @@
+package GameEngine.World.Projectiles;
+
+import GameEngine.World.Entity;
+
+public class Laser extends Entity {
+
+
+}
---
+++ src/main/java/GameEngine/World/Projectiles/Rocket.java
@@ -0,0 +1,9 @@
+package GameEngine.World.Projectiles;
+
+import GameEngine.World.Entity;
+import javafx.scene.canvas.GraphicsContext;
+
+public class Rocket extends Entity {
+
+
+}
