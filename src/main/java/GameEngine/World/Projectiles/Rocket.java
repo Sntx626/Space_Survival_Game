fc9eda9c3ecbbfa74db4f2c773ef2b0d6eecab02
Leonard Langer <syntaxzockt@gmail.com>
2021-04-18 17:36:26 +0200

    Added Rocket Sprite

--- src/main/java/GameEngine/World/Projectiles/Rocket.java
+++ src/main/java/GameEngine/World/Projectiles/Rocket.java
@@ -5,6 +5,7 @@ import GameEngine.World.Entity;
 import GameEngine.World.Entitys.AstroidPiece;
 import GameEngine.World.Vector;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -13,6 +14,8 @@ public class Rocket extends Entity {
     Frame frame;
     Entity belongTo;
     int damage = 100;
+    double lastAngle;
+    boolean angleSet = false;
 
     public Rocket(Frame frame, Entity belongTo) {
         super(frame);
@@ -22,6 +25,8 @@ public class Rocket extends Entity {
         this.setY(belongTo.getY());
         this.setH(32);
         this.setW(32);
+        Image img = this.getFrame().getWorld().getImageCache().getImage("file:rsc/entity_data/rocket.png");
+        this.setSprite(img);
         this.setMaxSpeed(200);
         this.setCanCollide(true);
         this.setHasPushback(false);
@@ -30,12 +35,21 @@ public class Rocket extends Entity {
     @Override
     public void render(GraphicsContext gc, int cx, int cy, int cw, int ch, int w, int h, int mx, int my) {
 
+        if (!this.angleSet) {
+            lastAngle = belongTo.getAngleToMouse(cx, cy, cw, ch, w, h, mx, my);
+            this.angleSet = true;
+        }
+
         Vector pos = this.getViewPortCords(cx, cy, cw, ch, w, h);
         Vector size = this.getViewPortSize(cx, cy, cw, ch, w, h);
 
         gc.save();
-        gc.setFill(Color.GREENYELLOW);
-        gc.fillRect(pos.getX() - size.getX()/2 ,pos.getY() - size.getY()/2, size.getX(), size.getY());
+        gc.translate(pos.getX(), pos.getY());
+        gc.rotate(lastAngle);
+        //gc.setFill(Color.GREENYELLOW);
+        //gc.fillRect(pos.getX() - size.getX()/2 ,pos.getY() - size.getY()/2, size.getX(), size.getY());
+        gc.drawImage(this.getSprite(), -(size.getX())/2, -(size.getY())/2, size.getX()*2, size.getY()*2);
+        //gc.drawImage(this.getSprite(), pos.getX() - size.getX()/2, -pos.getY() - size.getY()/2, size.getX(), size.getY());
         gc.restore();
     }
 
